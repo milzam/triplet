9c8b990112e15ddd2470fd09d210035e41fda5fa
Milzam Abi Karami <milzamabikarami@yahoo.com>
2017-11-28 00:09:00 +0700

    Work In Progress

---
+++ triplet/ViewController/ItemListScreen.swift
@@ -0,0 +1,13 @@
+//
+//  ItemListScreen.swift
+//  triplet
+//
+//  Created by Milzam Abi Karami on 26/11/17.
+//  Copyright © 2017 VestaMilzam. All rights reserved.
+//
+
+import UIKit
+
+class ItemListScreen: UITableViewController {
+    
+}
